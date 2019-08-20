ca3dafcdcb03fbb3487e8dbea7e407a46c938614
Grace <gsyed1120@gmail.com>
2019-08-20 22:46:00 +0100

    added child theme files

---
+++ twentynineteen-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Twenty Nineteen Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Twenty_Nineteen
+* @since 1.0.0
+*/
+
+// Enqueue scripts and styles
+function twentynineteen_child_scripts(){
+	wp_enqueue_style( 'twentynineteen-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'twentynineteen-style' ));
+}
+add_action( 'wp_enqueue_scripts', 'tribe_child_scripts' );
---
+++ twentynineteen-child/header.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Tech Sisters</title>
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato|Nunito+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
+
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+		    <a class=navbar-brand href="http://www.tech-sisters.com">
+		      <img src="http://www.tech-sisters.com/wp-content/uploads/2019/08/logo.png" class="logo" alt="Tech Sisters Logo">
+		    </a>
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse navbar-links-container" id="navbarSupportedContent">
+		      <ul class="navbar-nav">
+		        <li class="nav-item active">
+		          <a class="nav-link" href="http://www.tech-sisters.com">Home<span class="sr-only">(current)</span> </a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="http://www.tech-sisters.com/about">About</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="http://www.tech-sisters.com/articles">Articles</a>
+		        </li>
+		      </ul>
+		    </div>
+		  </nav>
+		</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
